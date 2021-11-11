789bcfbbde3f054b3db325decd0170bae03c490b
MAhdiKTU <mahdiallafi1@gmail.com>
2021-11-11 18:00:50 +0200

    add:welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
-        
+        {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}" /> --}}
+        <link href="css/app.css" rel="stylesheet">
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
@@ -128,8 +128,8 @@
                 </div>
             </div>
         </div> --}}
-     <script src="{{ mix('js/app.js') }}"></script>
-       
+     {{-- <script src="{{ mix('js/app.js') }}"></script> --}}
+     <script src="/js/app.js"></script> 
     </body>
     
 </html>
